9cc934a43644ccd3accf0b2beeb4f01b34b20745
bmartinyeye99 <forcemarci@gmail.com>
2023-11-14 10:32:50 +0100

    changing main

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ import time
 def get_dataframe_size_megabytes(dataframe):
     return dataframe.memory_usage(deep=True).sum() / (1024 * 1024)
 
-def crawl(url):
+def crawl(url,responses_df):
     hdr = {
         'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Mobile Safari/537.36',
         'Accept-Language': 'en-US'
     }
     response = requests.get(url, headers=hdr)
-    max_size_mb = 7000
+    max_size_mb = 500
     crawled_links = set()
     queue = deque([(url, 0)])
     while queue:
@@ -42,7 +42,6 @@ def crawl(url):
                     if current_url not in responses_df['URL'].values:
                         responses_df.loc[len(responses_df.index)] = [current_url, html_content]
                     df_size_mb = get_dataframe_size_megabytes(responses_df)
-                    print(df_size_mb)
                     if df_size_mb >= max_size_mb:
                         print(f"DataFrame size exceeded {max_size_mb} MB. Stopping crawl.")
                         break
@@ -81,26 +80,22 @@ def is_valid_url(url):
                        'episodes']
 
     movie_tv_pattern = r'https://www\.imdb\.com/title/[^"\']*'
-    person_pattern = r'https://www.imdb.com/name/[^"\']*'
     if re.match(movie_tv_pattern, url) and not any(word in url for word in restrictedWords):
         return 1
-    elif re.match(person_pattern, url):
-        return 0
     else:
         return -1
 
 
-def create_new_row(url,titleMatch,directors,matches):
-    if directors:
-        directors = directors.strip()
-    new_row = {'url': url.strip(), 'title': titleMatch.group(1).strip(), 'director': directors, 'cast': matches}
+def create_new_row(url,titleMatch,directors,matches,movie_tv_df):
+    if directors and titleMatch and url:
+        new_row = {'url': url.strip(), 'title': titleMatch.group(1).strip(), 'director': directors, 'cast': matches}
     if new_row['director'] == '{directorsOrCreatorsString}' or '{directorsOrCreatorsString}' in directors:
         new_row.update({'director': 'more'})
     if new_row['url'] not in movie_tv_df['url'].values:
         if new_row['director'] not in movie_tv_df['director'].values and new_row['title'] not in movie_tv_df['title']:
             movie_tv_df.loc[len(movie_tv_df)] = new_row
 
-def movie_series_parser(url,html_content):
+def movie_series_parser(url,html_content,movie_tv_df):
     if 'title' in url:
         titleReg = r'<title>(.*?)\((.*?) - IMDb<\/title'
         directorReg = r'Directed by (.*?)(?=\.\sWith)'
@@ -126,17 +121,10 @@ def movie_series_parser(url,html_content):
             if matches:
                 matches.pop()
 
-        create_new_row(url,titleMatch,directors,matches)
-
-        # new_row = {'url': url.strip(),  'title': titleMatch.group(1).strip(), 'director': directors.strip(), 'cast': matches}
-        # if new_row['director'] == '{directorsOrCreatorsString}' or '{directorsOrCreatorsString}' in directors:
-        #     new_row.update({'director': 'more'})
-        # if new_row['url'] not in movie_tv_df['url'].values:
-        #     if new_row['director'] not in movie_tv_df['director'].values and new_row['title'] not in movie_tv_df['title']:
-        #         movie_tv_df.loc[len(movie_tv_df)] = new_row
+        create_new_row(url,titleMatch,directors,matches,movie_tv_df)
 
 
-def parse(df):
+def parse(df,movie_tv_df):
     for index, row in df.iterrows():
         url_value = df['URL'].iloc[index]
         html_content = df['HTML_Content'].iloc[index]
@@ -146,36 +134,33 @@ def parse(df):
         if tmp == -1:
             continue
         elif tmp == 1:
-            movie_series_parser(url_value,html_content)
-        elif tmp == 0:
-            movie_series_parser(url_value,html_content)
-
-url = 'https://www.imdb.com/title/tt0104257/?ref_=nv_sr_srsg_3_tt_8_nm_0_q_for%2520a%2520few%2520'
-
-# if os.path.exists('extraction_movies.csv'):
-#     movie_tv_df = pd.read_csv("extraction_movies.csv",encoding='utf-8')
-# else:
-#     movie_tv_df = pd.DataFrame(columns=['url', 'title', 'director', 'cast'])
+            movie_series_parser(url_value,html_content,movie_tv_df)
 
 
-# if os.path.exists('web_data2.csv'):
-#     responses_df = pd.read_csv('web_data2.csv')
+def init_crawl(url):
+    responses_df = pd.DataFrame(columns=['URL', 'HTML_Content'])
+    movie_tv_df = pd.DataFrame(columns=['url', 'title', 'director', 'cast'])
+    crawl(url,responses_df)
+    parse(responses_df,movie_tv_df)
+    df_no_duplicates_subset = movie_tv_df.drop_duplicates(subset=['url', 'title'])
+    if os.path.exists('extraction_movies.csv'):
+        try:
+            df_no_duplicates_subset.to_csv('extraction_movies.csv', mode='a', index=False, header=False)
+        except Exception as e:
+            print(f"Error writing to CSV: {e}")
+    else:
+        df_no_duplicates_subset.to_csv('extraction_movies.csv', index=False)
+    return
 
-responses_df = pd.DataFrame(columns=['URL', 'HTML_Content'])
-movie_tv_df = pd.DataFrame(columns=['url', 'title', 'director', 'cast'])
+for i in range(2):
+    urls = ['https://www.imdb.com/title/tt8111088/?ref_=nv_sr_srsg_0_tt_8_nm_0_q_the%2520mand',
+            'https://www.imdb.com/title/tt4964310/?ref_=nv_sr_srsg_0_tt_4_nm_4_q_kincsem',
+            'https://www.imdb.com/title/tt0091042/?ref_=nv_sr_srsg_0_tt_2_nm_6_q_ferris',
+            'https://www.imdb.com/title/tt0335345/?ref_=nv_sr_srsg_1_tt_7_nm_0_q_passion',
+            'https://www.imdb.com/title/tt2442560/?ref_=nv_sr_srsg_0_tt_8_nm_0_q_peaky'
+            ]
+    init_crawl(urls[i])
 
-crawl(url)
 
-parse(responses_df)
-
-df_no_duplicates_subset = movie_tv_df.drop_duplicates(subset=['url', 'title'])
-
-if os.path.exists('extraction_movies.csv'):
-    try:
-        df_no_duplicates_subset.to_csv('extraction_movies.csv', mode='a', index=False,header=False)
-    except Exception as e:
-        print(f"Error writing to CSV: {e}")
-else:
-    df_no_duplicates_subset.to_csv('extraction_movies.csv', index=False)
 
 
